91c54dab528e5a943220f02d5ce67b6a21e64c20
Swayam Chatterjee <129827953+masterfighter999@users.noreply.github.com>
2023-11-09 11:39:43 +0530

    Add files via upload

---
+++ c_script.js
@@ -0,0 +1,28 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML == '=') {
+      string = eval(string);
+      document.querySelector('input').value = string;
+    } else if (e.target.innerHTML == 'C') {
+      string = "";
+      document.querySelector('input').value = string;
+    } else if (e.target.innerHTML == '%') {
+      // Calculate percentage
+      string = eval(string) / 100;
+      document.querySelector('input').value = string;
+    } else if (e.target.innerHTML == '√') {
+      // Square root
+      string = Math.sqrt(eval(string));
+      document.querySelector('input').value = string;
+    } else if (e.target.innerHTML == 'DE') {
+      // Delete last character
+      string = string.slice(0, -1);
+      document.querySelector('input').value = string;
+    } else {
+      string = string + e.target.innerHTML;
+      document.querySelector('input').value = string;
+    }
+  });
+});
